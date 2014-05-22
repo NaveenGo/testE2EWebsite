a9561525ede534e5e02db72365184c718839f213
unknown <ibraoz@ibraozbluepc.redmond.corp.microsoft.com>
2014-05-22 15:20:31 -0700

    trace added

--- Crash.aspx.cs
+++ Crash.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +10,9 @@ public partial class Crash : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        System.Diagnostics.Trace.TraceInformation("Information trace");
+        System.Diagnostics.Trace.TraceWarning("Warning trace");
+        System.Diagnostics.Trace.TraceError("Error trace");
         throw new Exception("Crashing the page for test purpose...");
     }
 }
