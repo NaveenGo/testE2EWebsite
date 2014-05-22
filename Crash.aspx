da10790ad5c14023aa70df3901778f0c2b02d730
unknown <ibraoz@ibraozbluepc.redmond.corp.microsoft.com>
2014-05-22 16:32:08 -0700

    trace added

--- Crash.aspx
+++ Crash.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Crash.aspx.cs" Inherits="Crash" %>
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Crash.aspx.cs" Inherits="Crash"%>
 
 <!DOCTYPE html>
 
@@ -10,6 +10,20 @@
     <form id="form1" runat="server">
     <div>
     
+        <asp:Button ID="TraceError" runat="server" Text="Trace Error" OnClick="TraceError_Click" Width="150px" />
+    
+        <br />
+        <br />
+        <asp:Button ID="TraceWarning" runat="server" Text="Trace Warning" OnClick="TraceWarning_Click" Width="150px" />
+    
+        <br />
+        <br />
+        <asp:Button ID="TraceInfo" runat="server" Text="Trace Information" OnClick="TraceInfo_Click" Width="150px" />
+        <br />
+        <br />
+        
+        <asp:Button ID="CrashPage" runat="server" Text="Crash" OnClick="CrashPage_Click" Width="150px" />
+
     </div>
     </form>
 </body>
--- Crash.aspx.cs
+++ Crash.aspx.cs
@@ -1,19 +1,36 @@
-﻿using System;
+﻿#define TRACE
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System;
+using System.Globalization;
 
 public partial class Crash : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        System.Diagnostics.Trace.TraceInformation("Information trace");
-        System.Diagnostics.Trace.TraceWarning("Warning trace");
-        System.Diagnostics.Trace.TraceError("Error trace");
-        System.Diagnostics.Trace.Flush();
+
+    }
+
+    protected void TraceError_Click(object sender, EventArgs e)
+    {
+        System.Diagnostics.Trace.TraceError(string.Format("Error trace at {0}", DateTime.Now.ToString(CultureInfo.CurrentUICulture)));
+    }
+    protected void TraceWarning_Click(object sender, System.EventArgs e)
+    {
+        System.Diagnostics.Trace.TraceWarning(string.Format("Warning trace at {0}", DateTime.Now.ToString(CultureInfo.CurrentUICulture)));
+    }
+    protected void TraceInfo_Click(object sender, System.EventArgs e)
+    {
+        System.Diagnostics.Trace.TraceInformation(string.Format("Information trace at {0}", DateTime.Now.ToString(CultureInfo.CurrentUICulture)));
+    }
+    protected void CrashPage_Click(object sender, System.EventArgs e)
+    {
         throw new Exception("Crashing the page for test purpose...");
     }
 }
