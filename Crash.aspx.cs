a49a0ca1d300c4a318f32aaed8f05b5364745813
unknown <ibraoz@ibraozbluepc.redmond.corp.microsoft.com>
2014-05-22 15:32:44 -0700

    trace issue fixed

--- Crash.aspx.cs
+++ Crash.aspx.cs
@@ -13,6 +13,7 @@ public partial class Crash : System.Web.UI.Page
         System.Diagnostics.Trace.TraceInformation("Information trace");
         System.Diagnostics.Trace.TraceWarning("Warning trace");
         System.Diagnostics.Trace.TraceError("Error trace");
+        System.Diagnostics.Trace.Flush();
         throw new Exception("Crashing the page for test purpose...");
     }
 }
